262db562ea01915e9587da9a165790b4f95a434f
Hussain512 <shujaatfida512@gmail.com>
2026-05-16 12:48:44 +0700

    Implement cetakDokter method in pasien class

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -35,3 +35,11 @@ public:
 void pasien::tambahDokter(dokter* pDokter) {
     daftar_dokter.push_back(pDokter);
 }
+
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) { // auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diterima
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
