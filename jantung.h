f669374b5b026bd68273a869a23cb5411810b206
Hussain512 <shujaatfida512@gmail.com>
2026-05-16 12:34:37 +0700

    Add jantung class with constructor and destructor

--- jantung.h
+++ jantung.h
@@ -1,3 +1,20 @@
 #pragma once
 #ifndef JANTUNG_H
 #define JANTUNG_H
+
+#include <iostream>
+
+using namespace std;
+
+class jantung {
+public:
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+
+#endif
