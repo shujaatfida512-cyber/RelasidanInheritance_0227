c548378cd7a889bd375fa3f1eae6cec54b4f77c0
Hussain512 <shujaatfida512@gmail.com>
2026-05-16 12:53:44 +0700

    Test pelajar inheritance and methods in main function

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -30,3 +30,10 @@ public:
         return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n";
     }
 };
+s
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+    return 0;
+}
