df7ead0c8de0c4d123e7fa9123460e958137f4a5
Hussain512 <shujaatfida512@gmail.com>
2026-05-16 12:47:19 +0700

    Create dokter class and patient relationship

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -17,3 +17,17 @@ public:
     void tambahDokter(dokter*);
     void cetakDokter();
 };
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
