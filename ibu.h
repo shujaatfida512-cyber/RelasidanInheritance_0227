9dba65ed1439b37d95de8bc8ed2694fc4e4ff4a5
Hussain512 <shujaatfida512@gmail.com>
2026-05-16 12:26:23 +0700

    Add ibu class with constructor and destructor

--- ibu.h
+++ ibu.h
@@ -5,3 +5,18 @@
 #include <vector>
 #include <string>
 #include "anak.h"
+
+using namespace std;
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
