108bfdc66a231d751fda21f241f5b2250af797d8
Hussain512 <shujaatfida512@gmail.com>
2026-05-16 12:26:56 +0700

    Add child printing function to ibu class

--- ibu.h
+++ ibu.h
@@ -20,3 +20,15 @@ public:
     ~ibu() {
         cout << "Ibu \"" << nama << "\" tidak ada\n";
     }
+    void cetakAnak() {
+        cout << "Daftar Anak dari Ibu \"" << nama << "\":" << endl;
+
+        for (int i = 0; i < daftar_anak.size(); i++) {
+            cout << daftar_anak[i]->nama << endl;
+        }
+
+        cout << endl;
+    }
+};
+
+#endif
