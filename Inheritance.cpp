aba47a2cf00ac8728abbf4e2e16dd550cfae86f4
Hussain512 <shujaatfida512@gmail.com>
2026-05-16 12:53:00 +0700

    Create pelajar class with inheritance from orang

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -16,3 +16,17 @@ public:
         return a + b;
     }
 };
+
+class pelajar : public orang {
+public:
+    string sekolah;
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n";
+    }
+};
